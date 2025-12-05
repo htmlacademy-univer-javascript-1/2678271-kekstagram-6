67e0bac98a67bb37d332da4de2029b3d46e9c5ad
Yana Koryakina <y.koryakina@g.nsu.ru>
2025-12-05 15:54:52 +0700

    Модуль pictures.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-import {createPhotoData, OBJECTS_COUNT} from './data.js';
+import {createPictures} from './data.js';
+import './pictures.js';
 
-const photoArray = Array.from({ length: OBJECTS_COUNT }, createPhotoData);
-
-console.log(photoArray);
+console.log(createPictures());
---
+++ js/pictures.js
@@ -0,0 +1,22 @@
+import {createPictures} from './data.js';
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const picturesBlock = document.querySelector('.pictures');
+
+const similarListFragment = document.createDocumentFragment();
+
+const similarPictures = createPictures();
+
+similarPictures.forEach(({url, description, likes, comments}) => {
+  const pictureElement = pictureTemplate.cloneNode(true);
+  pictureElement.querySelector('.picture__img').src = url;
+  pictureElement.querySelector('.picture__img').alt = description;
+
+
+  pictureElement.querySelector('.picture__likes').textContent = likes;
+  pictureElement.querySelector('.picture__comments').textContent = comments.length;
+
+  similarListFragment.appendChild(pictureElement);
+});
+
+picturesBlock.appendChild(similarListFragment);
+
