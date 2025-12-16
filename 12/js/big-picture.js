4537fc7acfe92be0cd595afdc34fcbfd86c2bc6f
Keks <keks@htmlacademy.ru>
2025-12-16 20:58:40 +0300

    :heavy_check_mark: Сборка #12

--- 12/js/big-picture.js
+++ 12/js/big-picture.js
@@ -56,6 +56,9 @@ function renderCommentsFragment(comments) {
     commentsLoader.classList.add('hidden');
     commentCountBlock.classList.add('hidden');
     return;
+  }else{
+    commentsLoader.classList.remove('hidden');
+    commentCountBlock.classList.remove('hidden');
   }
 
   renderMoreComments(comments, fragment, 0);
